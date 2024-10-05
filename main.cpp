f580af58017374edf5d442ca37630f434fcdb105
Erick Pascual-Bautista <89057539+lemmeBozo@users.noreply.github.com>
2024-10-04 17:41:53 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
 
     // add any code here, sort of a 'hello world plus' code block
+    cout << "didn't we do this assignment already?" << endl;
 
     return 0;
 }
